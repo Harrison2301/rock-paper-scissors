282bac6e763a9460a9dbccab0df64eb5c6dd8240
Harrison2301 <harrisonwinters2301@gmail.com>
2019-08-28 22:22:04 +0000

    forgot to link script to html

--- app.js
+++ app.js
@@ -7,3 +7,7 @@
  const rock_div = document.getElementById("r");
  const paper_div = document.getElementById("p");
  const scissors_div = document.getElementById("s");
+
+ rock_div.addEventListener('click', function() {
+     console.log("test")
+ })
